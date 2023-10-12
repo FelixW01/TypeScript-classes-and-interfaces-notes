8dc990543ba0fb482d67bcc253866c09d6284357
Felix W <felixwillem01@yahoo.com>
2023-10-12 19:49:44 -0400

    worked on inheritance

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 class Department {
-  private name: string;
-  //only methods within the class can affect the "private" employees
+  // private id: string;
+  //   private name: string;
+  //only methods within the class can affect the "private" employees, "readonly" will stop it from being changed.
   private employees: string[] = [];
-  constructor(n: string) {
-    this.name = n;
+  constructor(private readonly id: string, public name: string) {
+    // this.name = n;
+    // this.id= id;
   }
   describe(this: Department) {
-    console.log("Department: " + this.name);
+    console.log(`Department (${this.id}) : ${this.name}`);
   }
   addEmployee(employee: string) {
     this.employees.push(employee);
@@ -17,12 +19,36 @@ class Department {
   }
 }
 
-const business = new Department("Business");
-business.addEmployee("Felix Willem");
-business.addEmployee("Jillian");
-business.printEmployeeInformation();
+class ITDepartment extends Department {
+  constructor(id: string, public admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
+}
 
-business.describe();
+class BusinessDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, "IT");
+  }
 
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const it = new ITDepartment("d1", ["Felix"]);
+it.addEmployee("Felix Willem");
+it.addEmployee("Jillian");
+it.printEmployeeInformation();
+console.log(it);
+it.describe();
+
+const business = new BusinessDepartment("d2", []);
+business.addReport("Something went wrong...");
+business.printReports();
 // const businessCopy = { name: "DUMMY", describe: business.describe };
 // businessCopy.describe();
