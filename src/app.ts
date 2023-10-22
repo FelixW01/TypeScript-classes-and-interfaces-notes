c1821afbe3885d60890a5b6448f6ef6bbabde12d
Felix W <felixwillem01@yahoo.com>
2023-10-22 17:55:31 -0400

    worked on multiple interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
-// Can be implimented by class to provide type checking of objects
-interface Greetable {
-  name: string;
+interface Named {
+  // Created object name is instantiated once and is readonly, can't be changed after
+  readonly name: string;
+}
 
+// Can be implimented by class to provide type checking of objects
+// You can extend an interface with another interface, extending multiple interfaces is possible by a ,
+interface Greetable extends Named {
   greet(phrase: string): void;
 }
 // You can pass multiple interfaces on a class
@@ -24,5 +28,8 @@ let user1: Greetable;
 // User1 is constrained to being greetable, on top of the blueprint Person class making the
 // created object even more strict.
 user1 = new Person("Felix");
+// changing name throws an error because the name property is readonly.
+// user1.name = ('Max')
+
 user1.greet(" Hi there - I am ");
 console.log(user1);
