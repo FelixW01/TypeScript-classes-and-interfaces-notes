33145e0a05e0c78e24872e758d9dd0391bbf6cab
Felix W <felixwillem01@yahoo.com>
2023-10-22 19:51:51 -0400

    worked on intersection types

--- src/app.ts
+++ src/app.ts
@@ -1,49 +1,23 @@
-// type AddFn = (a: number, b: number) => number;
-// Interface can be used as an alternative option to type, custom types are usually more common
-interface AddFn {
-  (a: number, b: number): number;
-}
-
-let add: AddFn;
-
-add = (n1: number, n2: number) => {
-  return n1 + n2;
+type Admin = {
+  name: string;
+  privileges: string[];
 };
-interface Named {
-  // Created object name is instantiated once and is readonly, can't be changed after
-  readonly name?: string;
-  outputName?: string;
-}
 
-// Can be implimented by class to provide type checking of objects
-// You can extend an interface with another interface, extending multiple interfaces is possible by a ,
-interface Greetable extends Named {
-  greet(phrase: string): void;
-}
-// You can pass multiple interfaces on a class
-class Person implements Greetable {
-  name?: string;
-  age = 30;
-  constructor(n?: string) {
-    if (n) {
-      this.name = n;
-    }
-  }
+type Employee = {
+  name: string;
+  startDate: Date;
+};
 
-  greet(phrase: string) {
-    console.log(phrase + " " + this.name);
-  }
-}
+// interface ElevatedEmployee extends Employee, Admin {}
+type ElevatedEmployee = Admin & Employee;
 
-// User1 HAS to be greetable, having at least a name with a type of string and a greet method
-// with the input of phrase and an a return of nothing.
-let user1: Greetable;
+const e1: ElevatedEmployee = {
+  name: "Felix",
+  privileges: ["create0server"],
+  startDate: new Date(),
+};
 
-// User1 is constrained to being greetable, on top of the blueprint Person class making the
-// created object even more strict.
-user1 = new Person();
-// changing name throws an error because the name property is readonly.
-// user1.name = ('Max')
+type Combineable = string | number;
+type Numeric = number | boolean;
 
-user1.greet(" Hi there - I am ");
-console.log(user1);
+type Universal = Combineable & Numeric;
---
+++ src/intersection.ts
@@ -0,0 +1,49 @@
+// type AddFn = (a: number, b: number) => number;
+// Interface can be used as an alternative option to type, custom types are usually more common
+interface AddFn {
+  (a: number, b: number): number;
+}
+
+let add: AddFn;
+
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+interface Named {
+  // Created object name is instantiated once and is readonly, can't be changed after
+  readonly name?: string;
+  outputName?: string;
+}
+
+// Can be implimented by class to provide type checking of objects
+// You can extend an interface with another interface, extending multiple interfaces is possible by a ,
+interface Greetable extends Named {
+  greet(phrase: string): void;
+}
+// You can pass multiple interfaces on a class
+class Person implements Greetable {
+  name?: string;
+  age = 30;
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
+  }
+
+  greet(phrase: string) {
+    console.log(phrase + " " + this.name);
+  }
+}
+
+// User1 HAS to be greetable, having at least a name with a type of string and a greet method
+// with the input of phrase and an a return of nothing.
+let user1: Greetable;
+
+// User1 is constrained to being greetable, on top of the blueprint Person class making the
+// created object even more strict.
+user1 = new Person();
+// changing name throws an error because the name property is readonly.
+// user1.name = ('Max')
+
+user1.greet(" Hi there - I am ");
+console.log(user1);
