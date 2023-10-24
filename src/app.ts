1109a463377c19ed0a07564f5769cf59416924b4
Felix W <felixwillem01@yahoo.com>
2023-10-23 20:43:52 -0400

    worked with constraints

--- src/app.ts
+++ src/app.ts
@@ -7,3 +7,13 @@
 //     resolve("This is done!");
 //   }, 2000);
 // });
+
+// generic types offers less strict, but effective type checks
+// in this example, T & U is just placeholder for whatever type the input is, while
+// adding the costraint by extending the object forces the input to be an object the entire process.
+function merge<T extends Object, U extends Object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: "Felix", hobbies: ["Music"] }, { age: 23 });
+console.log(mergedObj);
