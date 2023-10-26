3cd8df995f86f15809f43819a08098047af2db69
Felix W <felixwillem01@yahoo.com>
2023-10-26 19:46:01 -0400

    worked with autobind decorator

--- src/app.ts
+++ src/app.ts
@@ -74,7 +74,7 @@ class Product {
   @Log
   title: string;
   private _price: number;
-  // Method Decorator
+  // Accessor Decorator
   @Log2
   set price(val: number) {
     if (val > 0) {
@@ -88,10 +88,34 @@ class Product {
     this.title = t;
     this._price = p;
   }
-  // Accessor Decorator
+  // Method Decorator
   @Log3
   // Parameter Decorator
   getPriceWithTax(@Log4 tax: number) {
     return this._price * (1 + tax);
   }
 }
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+class Printer {
+  message = "This Works!";
+
+  @Autobind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer();
+const button = document.querySelector("button")!;
+button.addEventListener("click", p.showMessage);
