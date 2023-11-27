d2cca8e44c5310b30ccd1ad96d399aa58223da74
Felix W <felixwillem01@yahoo.com>
2023-11-27 17:15:07 -0500

    worked with notes on react and typescript, initial commit

---
+++ src/first-app.js
@@ -0,0 +1,6 @@
+var userName = "Max";
+//userName
+userName = "Max";
+var userAge = 34;
+var isValid = true;
+// string, number, boolean
---
+++ src/first-app.ts
@@ -0,0 +1,10 @@
+let userName: string = "Max";
+
+//userName
+userName = "Max";
+
+let userAge = 34;
+
+let isValid = true;
+
+// string, number, boolean
